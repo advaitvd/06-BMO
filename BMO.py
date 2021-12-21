c61ac935f3b87cdde85a3be831d11607cc7225c4
advaitvd <advaitvdgoa@gmail.com>
2021-12-22 02:28:48 +0530

    algorithm run, sorting society step added

--- BMO.py
+++ BMO.py
@@ -9,6 +9,9 @@ class BMO:
         self.cost_fn = cost_fn
         self.society, self.fitness = self.initialize_population()
         self.max_generations = max_generations
+        self.mcf = 0.9
+        self.mcfp = 0.95
+        self.mu = 1e-2
 
     def initialize_population(self):
         population = self.lb + (self.ub - self.lb)*np.random.random(size = (self.population_size, self.n_vars))
@@ -17,6 +20,32 @@ class BMO:
             fitness[i] = self.cost_fn(population[i,:])
         
         return population, fitness
+    
+    def run(self):
+        for step in  range(1,1+self.max_generations):
+            order = self.fitness.argsort()
+            self.society = self.society[order,:]
+
+            # partition into males and females
+            # specify monogamous, polygynous and polyandrous birds
+            # remove the worst birds and generate promiscuous birds based on the chaotic sequence
+
+            # compute objective function of the promiscuous birds
+
+            # produce broods for monogamous birds
+
+            # produce broods for polygynous birds
+
+            # produce broods for polyandrous birds
+
+            # produce broods for promiscuous birds
+
+            # produce broods for parthenogenetic birds
+
+            # compute objective function of the broods
+
+            # replacement stage
+            # update parameters
 
 
 

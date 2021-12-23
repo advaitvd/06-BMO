f0bab5f7ed0622b438b3a9ba35bac6b9e7c74b21
advaitvd <advaitvdgoa@gmail.com>
2021-12-23 19:46:40 +0530

    functionality to plot the convergence plot added.

--- BMO.py
+++ BMO.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class BMO:
     def __init__(self, cost_fn, n_vars, population_size=100, max_generations=100, ub=1, lb=0):
@@ -10,8 +11,12 @@ class BMO:
         self.society, self.fitness = self.initialize_population()
         self.max_generations = max_generations
         self.mcf = 0.9
-        self.mcfp = 0.9
-        self.mu = 1e-2
+        
+        self.mcfp0 = 0.1
+        self.mcfpinf = 0.9
+        
+        self.mu = 1e-3
+        
         self.w0 = 2.5
         self.winf = 0.25
 
@@ -25,6 +30,8 @@ class BMO:
         self.best_ = None
         self.best_fitness_ = None
 
+        self.log = np.zeros((max_generations,))
+
     def initialize_population(self):
         population = self.lb + (self.ub - self.lb)*np.random.random(size = (self.population_size, self.n_vars))
         fitness = np.zeros((self.population_size,))
@@ -33,15 +40,21 @@ class BMO:
         
         return population, fitness
     
+    def plot_graph(self):
+        plt.grid()
+        plt.plot(self.log)
+        plt.show()
+    
     def run(self):
-        for step in  range(1,1+self.max_generations):
+        for step in range(1,1+self.max_generations):
             # sort birds
             order = self.fitness.argsort()
             self.society = self.society[order,:]
             self.fitness = self.fitness[order]
 
             self.best_, self.best_fitness_ = (self.society[0,:], self.fitness[0])
-            # print("{0} : {1}".format(step, self.best_fitness_))
+            print("{0} : {1}".format(step, self.best_fitness_))
+            self.log[step-1] = self.best_fitness_
             
             # males: monogamous, polygynous and promiscuous
             # females: parthenogenetic and polyandrous
@@ -60,11 +73,11 @@ class BMO:
             for i in range(len(promiscuous_fit)):
                 promiscuous_fit[i] = self.cost_fn(promiscuous[i,:])
 
-            w = self.w0 + (self.winf - self.w0) * (step-1) / self.max_generations
+            w = self.w0 + (self.winf - self.w0) * step / self.max_generations
 
             # produce broods for monogamous birds
             for i in range(len(monogamous)):
-                xb  = monogamous[i,:] + w * np.random.random(self.n_vars) * polyandrous[np.random.randint(0,len(polyandrous)),:]
+                xb  = monogamous[i,:] + w * np.random.random(self.n_vars) * (polyandrous[np.random.randint(0,len(polyandrous)),:] - monogamous[i,:])
                 if np.random.random() > self.mcf:
                     xb[np.random.randint(0,self.n_vars)] = self.lb - np.random.random() *(self.lb - self.ub)
                 
@@ -78,9 +91,10 @@ class BMO:
 
             # produce broods for polygynous birds
             for i in range(len(polygynous)):
-                sumation_term = np.random.random(self.n_vars) * polyandrous[np.random.randint(0,len(polyandrous)),:]
+                x = polygynous[i,:]
+                sumation_term = np.random.random(self.n_vars) * (polyandrous[np.random.randint(0,len(polyandrous)),:]-x)
                 for i in range (2):
-                    sumation_term += np.random.random(self.n_vars) * polyandrous[np.random.randint(0,len(polyandrous)),:]
+                    sumation_term += np.random.random(self.n_vars) * (polyandrous[np.random.randint(0,len(polyandrous)),:]-x)
 
                 xb  = polygynous[i,:] + w * sumation_term
                 xb[xb>self.ub] = self.ub
@@ -95,9 +109,10 @@ class BMO:
                 
             # produce broods for polyandrous birds
             for i in range(len(polyandrous)):
-                sumation_term = np.random.random(self.n_vars) * monogamous[np.random.randint(0,len(monogamous)),:]
+                x = polyandrous[i,:]
+                sumation_term = np.random.random(self.n_vars) * (monogamous[np.random.randint(0,len(monogamous)),:]-x)
                 for i in range (2):
-                    sumation_term += np.random.random(self.n_vars) * monogamous[np.random.randint(0,len(monogamous)),:]
+                    sumation_term += np.random.random(self.n_vars) * (monogamous[np.random.randint(0,len(monogamous)),:]-x)
 
                 xb  = polyandrous[i,:] + w * sumation_term
                 xb[xb>self.ub] = self.ub
@@ -112,7 +127,8 @@ class BMO:
 
             # produce broods for promiscuous birds
             for i in range(len(promiscuous)):
-                xb  = promiscuous[i,:] + w * np.random.random(self.n_vars) * polyandrous[np.random.randint(0,len(polyandrous)),:]
+                x = promiscuous[i,:]
+                xb  = promiscuous[i,:] + w * np.random.random(self.n_vars) * (polyandrous[np.random.randint(0,len(polyandrous)),:]-x)
                 xb[xb>self.ub] = self.ub
                 xb[xb<self.lb] = self.lb
                 if np.random.random() > self.mcf:
@@ -124,8 +140,9 @@ class BMO:
                     promiscuous[i,:] = xb
 
             # produce broods for parthenogenetic birds
+            mcfp = self.mcfp0 + (self.mcfpinf - self.mcfp0) * step / self.max_generations
             for i in range(len(parthenogetic)):
-                if np.random.random() > self.mcfp :
+                if np.random.random() > mcfp :
                     xb = parthenogetic[i,:]+ self.mu * (np.random.random(self.n_vars)-0.5) * parthenogetic[i,:]
                     xb[xb>self.ub] = self.ub
                     xb[xb<self.lb] = self.lb
@@ -133,21 +150,20 @@ class BMO:
                     if cost_val < parthenogetic_fit[i]:
                         parthenogetic_fit[i] = cost_val
                         parthenogetic[i,:] = xb
-            # compute objective function of the broods
 
             # replacement stage
             # update parameters
 
             self.society = np.concatenate((polyandrous, parthenogetic, monogamous, polygynous, promiscuous),axis = 0)
             self.fitness = np.concatenate((polyandrous_fit, parthenogetic_fit, monogamous_fit, polygynous_fit, promiscuous_fit))
-            print(self.fitness.mean())
 
 
 
 if __name__ == '__main__':
     def func(x):
         return np.sum(np.abs(x+0.5)**2)
-    test = BMO(cost_fn=func,n_vars=4,max_generations=10000, ub = 10, lb = -10)
+    test = BMO(cost_fn=func,n_vars=4,max_generations=100, ub = 10, lb = -10)
     test.run()
+    test.plot_graph()
 
     print(test.best_fitness_,test.best_)
